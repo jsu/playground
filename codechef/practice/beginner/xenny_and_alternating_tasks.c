84bf8766bfafd56ebfed83d27f9db08ff703527e
Jay Su <jsu317@gmail.com>
2017-03-14 11:27:01 +0800

    remove a.outs and update working xenny

--- codechef/practice/beginner/xenny_and_alternating_tasks.c
+++ codechef/practice/beginner/xenny_and_alternating_tasks.c
@@ -1,32 +1,51 @@
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE (10 * 1024 * 1024)
+char *BUFFER;
+
+static int parseint()
+{
+    extern char *BUFFER;
+    int n;
+
+    while(*BUFFER < 0x21)
+        BUFFER++;
+    for(n = 0; *BUFFER >= 0x21; BUFFER++)
+        n = n * 10 + *BUFFER - '0';
+    
+    return n;
+}
 
 int main(void)
 {
-    int t, n, i, r;
+    extern char *BUFFER;
+    char *buffer;
+    int t, n, i;
     uint64_t results[2];
 
-    scanf("%d", &t);
+    buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+    BUFFER = buffer;
+    fread(BUFFER, sizeof(char), BUFFER_SIZE, stdin);
+    t = parseint();
     while(t--)
     {
-        scanf("%d", &n); 
+        n = parseint();
         results[0] = 0;
         results[1] = 0;
         for(i = 0; i < n; i++)
-        {
-            scanf("%d", &r); 
-            results[i % 2] += r;
-        }
+            results[i % 2] += parseint();
         for(i = 1; i <= n; i++)
-        {
-            scanf("%d", &r); 
-            results[i % 2] += r;
-        }
+            results[i % 2] += parseint();
 
         if(results[0] > results[1])
-            printf("%llu", results[1]);
+            printf("%" PRIu64 "\n", results[1]);
         else
-            printf("%llu", results[0]);
+            printf("%" PRIu64 "\n", results[0]);
     }
+    free(buffer);
     return 0;
 }
