095db9fba82d514d933f3bcb510b7919f0f30274
jsu <jsu317@gmail.com>
2018-12-04 21:55:33 -0800

    fix indentation

--- leetcode/two_sum_hashtable.c
+++ leetcode/two_sum_hashtable.c
@@ -5,21 +5,21 @@
 
 typedef struct hash_item_s
 {
-	int key;
-	int value;
-	struct hash_item_s *next;
+    int key;
+    int value;
+    struct hash_item_s *next;
 }hash_item_t;
 
 typedef struct hash_table_s
 {
-	hash_item_t **array;
+    hash_item_t **array;
 }hash_table_t;
 
 void put(hash_table_t *table, int key, int value)
 {
     hash_item_t *item, *prev;
-	size_t index = (size_t)key;	
-	index %= TABLE_SIZE;
+    size_t index = (size_t)key;	
+    index %= TABLE_SIZE;
 
     if(table->array[index] == NULL)
     {
@@ -61,7 +61,7 @@ int get(hash_table_t *table, int key)
     index %= TABLE_SIZE;
 
     if(table->array[index] == NULL)
-	    return -1;
+        return -1;
 
     item = table->array[index];
     while(item)
@@ -78,25 +78,25 @@ int get(hash_table_t *table, int key)
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target) {
-	int i, j, *result;
-	hash_table_t *table;
-	table = (hash_table_t *)malloc(sizeof(table));
-	table->array = (hash_item_t **)calloc(TABLE_SIZE, sizeof(hash_item_t *));
+    int i, j, *result;
+    hash_table_t *table;
+    table = (hash_table_t *)malloc(sizeof(table));
+    table->array = (hash_item_t **)calloc(TABLE_SIZE, sizeof(hash_item_t *));
 
-	result = (int *)malloc(sizeof(result) * 2);
+    result = (int *)malloc(sizeof(result) * 2);
 
-	for(i=0; i<numsSize; i++)
-	{
-		if((j = get(table, target - nums[i])) != -1)
-		{
-			result[0] = j;
-			result[1] = i;
-		}
-		else
-			put(table, nums[i], i);
-	}
+    for(i=0; i<numsSize; i++)
+    {
+        if((j = get(table, target - nums[i])) != -1)
+        {
+            result[0] = j;
+            result[1] = i;
+        }
+        else
+            put(table, nums[i], i);
+    }
 
-	return result;
+    return result;
 }
 
 int main(int argc, char **argv)
@@ -104,5 +104,5 @@ int main(int argc, char **argv)
     int a[] = {2, 7, 11, 15}, *r;
     r = twoSum(a, 4, 9);
     printf("%d %d\n", r[0], r[1]);
-	return 0;
+    return 0;
 }
