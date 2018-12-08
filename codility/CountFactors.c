c4f855b6e3df0455f8b7279497774ef7394744f8
jsu <jsu317@gmail.com>
2018-12-08 09:04:07 -0800

    Codility CountFactors

--- codility/CountFactors.c
+++ codility/CountFactors.c
@@ -1,16 +1,25 @@
 // you can write to stdout for debugging purposes, e.g.
 // printf("this is a debug message\n");
+#include <math.h>
 
 int solution(int N) {
     // write your code in C99 (gcc 6.2.0)
-    int i, count;
+    int i, count, sqrt_N;
+    
+    if(N == 1)
+        return 1;
+        
     count = 0;
-    for(i = 1; i * i < N; i++)
+    /* 
+     * precaculate sqrt(N) to avoid i * i < N being calc'd every time
+     */
+    sqrt_N = sqrt(N);
+    for(i = 1; i <= sqrt_N; i++)
         if(N % i == 0)
             count += 2;
     
-    if(i * i == N)
-        count++;
-
+    if(sqrt_N * sqrt_N == N)
+        count--;
+        
     return count;
 }
